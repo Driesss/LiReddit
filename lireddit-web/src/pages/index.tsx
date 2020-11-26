008963262175f53fa89fe12da0356d1e912af8e9
Dries Stelten <dries.stelten@telenet.be>
2020-11-26 15:01:44 +0100

    Implement cursor pagination on posts

--- lireddit-web/src/pages/index.tsx
+++ lireddit-web/src/pages/index.tsx
@@ -1,12 +1,22 @@
-import { Box, Flex, Heading, Link, Stack, Text } from '@chakra-ui/core';
+import { Box, Button, Flex, Heading, Link, Stack, Text } from '@chakra-ui/core';
 import { withUrqlClient } from 'next-urql';
 import NextLink from 'next/link';
+import { useState } from 'react';
 import { Layout } from '../components/Layout';
 import { usePostsQuery } from '../generated/graphql';
 import { createUrqlClient } from '../utils/createUrqlClient';
 
 const Index = () => {
-    const [{ data }] = usePostsQuery({ variables: { limit: 25 } });
+    const [variables, setVariables] = useState({
+        limit: 10,
+        cursor: null as string | null,
+    });
+    const [{ data, fetching }] = usePostsQuery({ variables });
+
+    if (!fetching && !data) {
+        return <div> you get no posts for some reason</div>;
+    }
+
     return (
         <Layout>
             <Flex align="center">
@@ -16,11 +26,11 @@ const Index = () => {
                 </NextLink>
             </Flex>
             <br />
-            {!data ? (
+            {!data && fetching ? (
                 <div>loading...</div>
             ) : (
                 <Stack spacing={8}>
-                    {data.posts.map((p) => (
+                    {data!.posts.map((p) => (
                         <Box p={5} key={p.id} shadow="md" borderWidth="1px">
                             <Heading fontSize="xl">{p.title}</Heading>
                             <Text mt={4}>{p.textSnippet}</Text>
@@ -28,6 +38,24 @@ const Index = () => {
                     ))}
                 </Stack>
             )}
+            {data ? (
+                <Flex>
+                    <Button
+                        onClick={() => {
+                            setVariables({
+                                limit: variables.limit,
+                                cursor:
+                                    data.posts[data.posts.length - 1].createdAt,
+                            });
+                        }}
+                        isLoading={fetching}
+                        m="auto"
+                        my={8}
+                    >
+                        load more
+                    </Button>
+                </Flex>
+            ) : null}
         </Layout>
     );
 };
