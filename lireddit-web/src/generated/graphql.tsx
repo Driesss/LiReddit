e2b3f9301562c7b0b1ad70777c3af44bd084578f
Dries Stelten <dries.stelten@telenet.be>
2020-11-26 01:37:13 +0100

    add styling to index page

--- lireddit-web/src/generated/graphql.tsx
+++ lireddit-web/src/generated/graphql.tsx
@@ -40,6 +40,7 @@ export type Post = {
   points: Scalars['Float'];
   createdAt: Scalars['String'];
   updatedAt: Scalars['String'];
+  textSnippet: Scalars['String'];
 };
 
 export type User = {
@@ -238,7 +239,7 @@ export type PostsQuery = (
   { __typename?: 'Query' }
   & { posts: Array<(
     { __typename?: 'Post' }
-    & Pick<Post, 'id' | 'createdAt' | 'updatedAt' | 'title'>
+    & Pick<Post, 'id' | 'createdAt' | 'updatedAt' | 'title' | 'textSnippet'>
   )> }
 );
 
@@ -350,6 +351,7 @@ export const PostsDocument = gql`
     createdAt
     updatedAt
     title
+    textSnippet
   }
 }
     `;
--- lireddit-web/src/pages/index.tsx
+++ lireddit-web/src/pages/index.tsx
@@ -1,22 +1,32 @@
+import { Box, Flex, Heading, Link, Stack, Text } from '@chakra-ui/core';
 import { withUrqlClient } from 'next-urql';
-import { createUrqlClient } from '../utils/createUrqlClient';
-import { usePostsQuery } from '../generated/graphql';
-import { Layout } from '../components/Layout';
-import { Link } from '@chakra-ui/core';
 import NextLink from 'next/link';
+import { Layout } from '../components/Layout';
+import { usePostsQuery } from '../generated/graphql';
+import { createUrqlClient } from '../utils/createUrqlClient';
 
 const Index = () => {
     const [{ data }] = usePostsQuery({ variables: { limit: 25 } });
     return (
         <Layout>
-            <NextLink href="/create-post">
-                <Link>create post</Link>
-            </NextLink>
+            <Flex align="center">
+                <Heading>LiReddit</Heading>
+                <NextLink href="/create-post">
+                    <Link ml="auto">create post</Link>
+                </NextLink>
+            </Flex>
             <br />
             {!data ? (
                 <div>loading...</div>
             ) : (
-                data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+                <Stack spacing={8}>
+                    {data.posts.map((p) => (
+                        <Box p={5} key={p.id} shadow="md" borderWidth="1px">
+                            <Heading fontSize="xl">{p.title}</Heading>
+                            <Text mt={4}>{p.textSnippet}</Text>
+                        </Box>
+                    ))}
+                </Stack>
             )}
         </Layout>
     );
