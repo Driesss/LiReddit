1f5f0233c6abe311f630f74d6c9b03634a999409
Dries Stelten <dries.stelten@telenet.be>
2020-11-27 15:15:49 +0100

    add post page

--- lireddit-web/src/components/NavBar.tsx
+++ lireddit-web/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Link } from '@chakra-ui/core';
+import { Box, Button, Flex, Heading, Link } from '@chakra-ui/core';
 import React from 'react';
 import NextLink from 'next/link';
 import { useLogoutMutation, useMeQuery } from '../generated/graphql';
@@ -31,7 +31,12 @@ const NavBar: React.FC<NavBarProps> = ({}) => {
         //user is logged in
     } else {
         body = (
-            <Flex>
+            <Flex align="center">
+                <NextLink href="/create-post">
+                    <Button as={Link} mr={4}>
+                        create post
+                    </Button>
+                </NextLink>
                 <Box mr={2}>{data?.me.username}</Box>
                 <Button
                     variant="link"
@@ -47,7 +52,14 @@ const NavBar: React.FC<NavBarProps> = ({}) => {
     }
     return (
         <Flex zIndex={1} position="sticky" top={0} bg="tan" p={4}>
-            <Box ml={'auto'}>{body}</Box>
+            <Flex flex={1} margin="auto" maxW={800} align="center">
+                <NextLink href="/">
+                    <Link>
+                        <Heading>LiReddit</Heading>
+                    </Link>
+                </NextLink>
+                <Box ml={'auto'}>{body}</Box>
+            </Flex>
         </Flex>
     );
 };
